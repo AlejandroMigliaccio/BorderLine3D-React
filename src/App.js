47f244cace1a83d5c393cff8e17c58e9a9e5d11f
AlejandroMigliaccio <102888662+AlejandroMigliaccio@users.noreply.github.com>
2022-08-29 19:02:20 -0300

    counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import { useState } from 'react';
+import ButtonCounter from './components/ButtonCounter/ButtonCounter';
 import Header from './components/Header/Header';
 import ItemListContainer from './components/ItemListContainer/ItemlistContainer';
 import NavBar from './components/NavBar/NavBar';
 function App() {
+  const stock=5;
   return (
     <div>
-      <Header></Header>
+      <Header/>
       <NavBar></NavBar>
       <ItemListContainer></ItemListContainer>
-      <div>
-        aksdlasmda
-      </div>
+      <ButtonCounter stock={stock} initial={1}></ButtonCounter>
     </div>
   );
 }

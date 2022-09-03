fcb9ae576e8fe8610ef8320a792b3437c6f947b8
AlejandroMigliaccio <102888662+AlejandroMigliaccio@users.noreply.github.com>
2022-09-03 19:55:26 -0300

    fuck

--- src/App.js
+++ src/App.js
@@ -3,28 +3,12 @@ import Header from './components/Header/Header';
 import ItemListContainer from './components/ItemListContainer/ItemlistContainer';
 import NavBar from './components/NavBar/NavBar';
 function App() {
-  const [productos, setProductos] = useState([])
-  const URL2="../../server/db/products"
-  const URL = "https://rickandmortyapi.com/api/character/?status=alive";
-
-  const getdata = () => {
-      fetch(URL)
-          .then((res) =>{
-              return res.json()
-          })
-          .then(data=> setProductos(data))
-          .catch(error=>console.error(error))
-  }
-
-  useEffect(() => {
-      getdata();
-  }, [])
 
   return (
     <div>
       <Header/>
       <NavBar></NavBar>
-      <ItemListContainer data={productos}></ItemListContainer>
+      <ItemListContainer></ItemListContainer>
     </div>
   );
 }
--- src/components/ItemListContainer/ItemlistContainer.jsx
+++ src/components/ItemListContainer/ItemlistContainer.jsx
@@ -1,12 +1,28 @@
 import ItemCard from "../ItemCard/ItemCard";
 
-const ItemListContainer = ({ productos }) => {
+const ItemListContainer = () => {
+    const [productos, setProductos] = useState([])
+    const URL2="../../server/db/products"
+    const URL = "https://rickandmortyapi.com/api/character/?status=alive";
+  
+    const getdata = () => {
+        fetch(URL)
+            .then((res) =>{
+                return res.json()
+            })
+            .then(data=> setProductos(data))
+            .catch(error=>console.error(error))
+    }
+  
+    useEffect(() => {
+        getdata();
+    }, [])
 
     return (
         <>
             {
                 productos.map((producto) => (
-                    <ItemCard data={producto}  ></ItemCard>
+                    <ItemCard data={producto} key={producto.id} ></ItemCard>
                 ))
             }
         </>
